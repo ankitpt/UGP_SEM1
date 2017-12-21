ad85b9997fb754accb2630b11378f4bde5577931
ankitpt <ankitpt@iitk.ac.in>
2017-12-22 00:21:43 +0530

    Add files via upload

--- cdr_quer.py
+++ cdr_quer.py
@@ -5,15 +5,17 @@ connection = sqlite3.connect("ct_id.db")
 
 cursor = connection.cursor()
 
-l=[1417,1418,1419]
+l=set(comb)
 
 sql_query = 'select * from CT_ID where CELL_ID in (' + ','.join(map(str, l)) + ')'
 
 cursor.execute(sql_query)
 
 l = cursor.fetchall()
-
+labels=['temp','Cell_ID','Lat','Long','Track_ID','Station_1','Station_2']
 
 connection.close()
 
-df = pd.DataFrame({'col':l})
+df = pd.DataFrame.from_records(l,columns=labels)
+del df['temp']
+df.to_csv('result.csv',index=False)
